60698612a1cd1c7197b40960bc4b73dc2ce298bc
Anton Vasilenko <antonv@remobjects.com>
2013-09-11 14:48:48 +0300

    0: sample server added

---
+++ Samples/SampleServer/NumericTextBoxValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SampleServer
+{
+    public class NumericTextBoxValidation: ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (String.IsNullOrEmpty((string) value))
+                return ValidationResult.ValidResult;
+            int dummy;
+            if (Int32.TryParse((string) value, out dummy))
+            {
+                if (dummy < 0 || dummy > 100)
+                    return new ValidationResult(false, "number should be between 0 and 100");
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Value is not of integer type");
+            }
+        }
+    }
+}
