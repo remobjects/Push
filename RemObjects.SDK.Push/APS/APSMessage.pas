7f028ab6e546334efcbda6271814bfad3d65ef6b
Anton Vasilenko <antonv@remobjects.com>
2013-09-27 12:53:18 +0300

    64279: RO Push: Refactor APSConnect to support events (step 1)

---
+++ RemObjects.SDK.Push/APS/APSMessage.pas
@@ -0,0 +1,47 @@
+﻿namespace RemObjects.SDK.Push.APS;
+
+interface
+
+uses
+  System,
+  System.Collections.Generic,
+  System.Linq,
+  System.Text;
+
+type
+  APSMessage = public class
+  public
+    property Identifier: Integer;
+    property DeviceToken: String;
+    property Expiration: DateTime;
+
+    property Payload: Payload;
+
+    constructor; empty;
+    constructor(aToken: String; aPayload: Payload := nil); empty;
+
+  end;
+
+  Payload nested in APSMessage = public class
+  public
+    property AlertSimple: String;
+    property Alert: Alert;
+    property Badge: nullable Integer;
+    property Sound: String;
+    property ContentAvailable: Boolean;
+    property Data: Dictionary<String, String>;
+    property HideActionButton: Boolean;  
+  end;
+
+  Alert nested in APSMessage = public class
+  public
+    property Body: String;
+    property ActionLocKey: String := nil;
+    property MessageLocKey: String;
+    property MessageLocArgs: array of String;
+    property LaunchImage: String;
+  end;
+
+implementation
+
+end.
